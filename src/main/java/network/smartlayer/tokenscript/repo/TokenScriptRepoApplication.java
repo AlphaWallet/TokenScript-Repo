277e6d7727707dc17c886f28b628a59c89d1fa2e
Weiwu Zhang <w@smarttokenlabs.com>
2023-08-07 12:24:52 +0200

    inital blank spring boot framework

---
+++ src/main/java/network/smartlayer/tokenscript/repo/TokenScriptRepoApplication.java
@@ -0,0 +1,13 @@
+package network.smartlayer.tokenscript.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenScriptRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenScriptRepoApplication.class, args);
+    }
+
+}
